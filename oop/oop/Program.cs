2f459932437121d172b97426143b9fb8e057adf5
vsesvit <sfrol91@gmail.com>
2023-08-25 17:30:08 +0200

    lesson

---
+++ oop/oop/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace oop;
+class Human
+{
+    public string name;
+    public int phoneNum;
+    public DateTime birthday;
+}
+
+
+class Program
+{
+   
+    static void Main(string[] args)
+    {
+        Human[] humans = new Human[3];
+
+        humans[0] = new Human();
+        humans[1] = new Human();
+        humans[2] = new Human();
+
+        humans[0].name = "Natalia";
+        humans[1].name = "Olga";
+        humans[2].name = "Katerina";
+
+
+        for(int i = 0; i < humans.Length; i++)
+        {
+
+        }
+        int[][] info = new int[4][];
+        info[0] = new int[3];
+        info[1] = new int[3];
+        info[2] = new int[3];
+        info[3] = new int[3];
+    
+
+        string[] name = new string[3];
+        int[] phoneNum = new int[3];
+        DateTime[] birthday = new DateTime[3];
+        name[0] = "Natalia";
+        name[1] = "Olga";
+        name[2] = "Katerina";
+        phoneNum[0] = 356536;
+        phoneNum[1] = 756896;
+        phoneNum[2] = 956532;
+        birthday[0] = new DateTime(1996, 4, 17);
+        birthday[1] = new DateTime(1990, 11, 17);
+        birthday[2] = new DateTime(1992, 5, 16);
+        
+
+    }
+}
+
